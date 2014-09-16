c97d6f2f1c4cdea0ec2f5cff998c2c73270a1fa8
Justin X. Lambeck <lambeck.j@gmail.com>
2014-09-16 15:51:38 +0200

    'hoi'

---
+++ weblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = 'ec822efeb37069dcff15e8ab8e198af24af8cc4ec2dac8f5a736d62b96319ff06745f1b78c925f36f43e742ed7bacc734383fee9d216b82c8f0aeb9abafa9fcd'
